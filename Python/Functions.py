8490e20b7d28d61b1684bb4d174c125ddf41e56c
Aidan <abachma3@u.rochester.edu>
2025-07-01 15:32:48 -0700

    Created file to store function definitions, defined computeUndulatorField function. Need to double check that the indexing is correct.

---
+++ Python/Functions.py
@@ -0,0 +1,15 @@
+# Description: Function definitions for simulation.
+
+# ---------- Imports ----------
+
+import numpy as np
+import Params as params
+import math
+
+# ---------- Function Definition ----------
+
+def computeUndulatorField(res_phase,Kz): # Compute undulator field, I guess
+    if params.tapering != 0: # Check if using tapering
+        startindex = max(1,math.floor(params.z0/params.stepsize))
+        res_phase[startindex:params.Nsnap] = params.psir # Set phase
+    Kz[0] = params.K # Store initial RMS K value in Kz array
--- Python/Main.py
+++ Python/Main.py
@@ -3,7 +3,17 @@
 # ---------- Imports ----------
 
 import Params as params # Parameters for code
+import Functions as funcs # Function definitions
+import numpy as np
 from numba import jit
 import time
 
-print(params.c)
+# ---------- Main Script ----------
+
+# Note: res_phase and Kz are numpy arrays (which are objects) so, by default, Python passes by reference. As a result,
+# we don't need any return statements on functions such as computeUndulatorField.
+
+res_phase = np.zeros([params.Nsnap]) # Resonant phase?
+Kz = np.zeros([params.Nsnap]) # Number of buckets for spatial discretization, I think
+
+funcs.computeUndulatorField(res_phase,Kz) # Compute undulator field
