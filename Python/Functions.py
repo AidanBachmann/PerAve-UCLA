4faf4d01ae8336080243226c6d6a11d34ce40fe7
Aidan <abachma3@u.rochester.edu>
2025-07-01 15:45:30 -0700

    Fixed indexing in computeUndulatorField.

--- Python/Functions.py
+++ Python/Functions.py
@@ -10,6 +10,6 @@ import math
 
 def computeUndulatorField(res_phase,Kz): # Compute undulator field, I guess
     if params.tapering != 0: # Check if using tapering
-        startindex = max(1,math.floor(params.z0/params.stepsize))
+        startindex = max(0,math.floor(params.z0/params.stepsize)) # Discretized initial position 
         res_phase[startindex:params.Nsnap] = params.psir # Set phase
     Kz[0] = params.K # Store initial RMS K value in Kz array
