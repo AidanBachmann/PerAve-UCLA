c1dfb11a33168a3d2af0831a2282e0656a546d89
Aidan <abachma3@u.rochester.edu>
2025-07-31 12:21:49 -0700

    Added functions for plotting field amplitude and phase, canonical particle position.

--- eta1D.m
+++
@@ -1,5 +0,0 @@
-% Compute the theoretical 1D relative energy loss along the undulator.
-function [eta_1d] = eta1D(param,bunch,zi)
-    JJ = besselj(0,param.chi) - besselj(1,param.chi);
-    eta_1d = ((param.chi2/param.gamma0)*((param.K*JJ*bunch)/(2*param.gamma0)))*(param.E0*zi + param.chistar*((param.K*JJ*bunch)/(2*param.gamma0))*zi.^2);
-end
